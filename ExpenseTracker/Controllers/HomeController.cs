c1f2e70e0c85c948997403ff1c6d075329f1cdf3
Grace <gralatta@gmail.com>
2024-12-21 02:30:49 -0600

    Edit and delete feat

--- ExpenseTracker/Controllers/HomeController.cs
+++ ExpenseTracker/Controllers/HomeController.cs
@@ -7,10 +7,12 @@ namespace ExpenseTracker.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ExpensesDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ExpensesDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -20,16 +22,51 @@ namespace ExpenseTracker.Controllers
 
         public IActionResult Expenses()
         {
+            // Go to DB _context, then Expenses table, track all values in ToList
+            var allExpenses = _context.Expenses.ToList();
+
+            var totalExpenses = allExpenses.Sum(x => x.Value);
+            ViewBag.Expenses = totalExpenses;
+
+            return View(allExpenses);
+        }
+
+        public IActionResult CreateEditExpense(int? id)
+        {
+            if(id != null)
+            {
+                // Editing -> load an expense by id
+                var expenseInDb = _context.Expenses.SingleOrDefault(expense => expense.Id == id);
+                return View(expenseInDb);
+            }
+
+            // If there's no id then return view
             return View();
         }
 
-        public IActionResult CreateEditExpense()
+        public IActionResult DeleteExpense(int id)
         {
-            return View();
+            var expenseInDb = _context.Expenses.SingleOrDefault(expense => expense.Id == id);
+            _context.Expenses.Remove(expenseInDb);
+            _context.SaveChanges();
+
+            // Return user to expenses page after deleting
+            return RedirectToAction("Expenses");
         }
 
         public IActionResult CreateEditExpenseForm(Expense model)
         {
+            if(model.Id == 0)
+            {
+                // Create
+                _context.Expenses.Add(model);
+            } else
+            {
+                // Edit
+                _context.Expenses.Update(model);
+            }
+            _context.SaveChanges();
+
             return RedirectToAction("Expenses");
         }
 
